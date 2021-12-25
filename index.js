7e1a4ba9a9446fd6a82438b1ce52741e36a587da
Elliot Robertson <elliotrobertson53@gmail.com>
2021-12-25 02:25:07 +0000

    added reset and music playing onLoad

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const customer = document.getElementById('customer');
 const amp = document.getElementById('amp');
 let scoreCount = document.getElementById('score');
+const music = document.getElementById('music');
 
+music.play();
 confirm(
   "'What it is mate'\n Can you avoid the Spider and finally get your refund?"
 );
+
+function reset() {
+  scoreCount.value = 0;
+  music.src = 'Assets/BOS.mp3';
+  music.play();
+}
 
 function jump() {
   customer.classList.add('animate');
@@ -14,6 +22,13 @@ function jump() {
       alert(
         'GAME OVER!!! \nWhats the verdict fella?...\n We only do credit notes'
       );
+      document.getElementById('music').src = '';
+    } else if (scoreCount.value == 10 || scoreCount.value == 30) {
+      document.getElementById('discount').style.visibility = 'visible';
+      scoreCount.value++;
+    } else if (scoreCount.value == 15 || scoreCount.value == 35) {
+      document.getElementById('discount').style.visibility = 'hidden';
+      scoreCount.value++;
     } else {
       scoreCount.value++;
     }
